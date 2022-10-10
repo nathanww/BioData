f396ba38a4de999dcedc62f016fa4be98ddd9957
nathanww <nathanww@gmail.com>
2022-10-10 09:25:14 -0400

    Basic setup stuff

--- app/src/main/java/com/neurelectrics/biodata/MainActivity.java
+++ app/src/main/java/com/neurelectrics/biodata/MainActivity.java
@@ -2,9 +2,30 @@ package com.neurelectrics.biodata;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.PowerManager;
+import android.os.StrictMode;
+import android.util.Log;
 import android.widget.TextView;
 
 import com.neurelectrics.biodata.databinding.ActivityMainBinding;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkResponse;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.HttpHeaderParser;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends Activity {
 
@@ -14,10 +35,74 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //allow internet accsess on UI thread
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        PowerManager powerManager = (PowerManager) getSystemService(POWER_SERVICE);
+        PowerManager.WakeLock wakeLock = powerManager.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK,
+                "BioData:dataAcquistion");
+        wakeLock.acquire();
+
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        mTextView = binding.text;
+    }
+
+
+    void sendData(String data, String userID) {
+
+        try {
+            RequestQueue requestQueue = Volley.newRequestQueue(this);
+            String URL = "https://biostream-1024.appspot.com/sendps";
+
+
+            StringRequest stringRequest = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    Log.i("VOLLEY", response);
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.e("VOLLEY", error.toString());
+                }
+            }) {
+                @Override
+                protected Map<String,String> getParams(){
+                    Map<String,String> params = new HashMap<String, String>();
+                    params.put("user",userID);
+                    params.put("data",data);
+                    Log.i("sleepdata",data);
+                    return params;
+                }
+
+                @Override
+                protected Response<String> parseNetworkResponse(NetworkResponse response) {
+                    String responseString = "";
+                    if (response != null) {
+                        responseString = String.valueOf(response.statusCode);
+                        // can get more details such as response.headers
+                    }
+                    return Response.success(responseString, HttpHeaderParser.parseCacheHeaders(response));
+                }
+            };
+            stringRequest.setRetryPolicy(new DefaultRetryPolicy(50 * 1000, 5, 1.0f));
+            requestQueue.add(stringRequest);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean checkInternet() { //returns true if internet is connected
+
+        try {
+            Socket sock = new Socket();
+            sock.connect(new InetSocketAddress("8.8.8.8", 53), 1500);
+            sock.close();
+
+
+            return true;
+        } catch (IOException e) { return false; }
     }
 }
